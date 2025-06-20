b24ccf3eef9c598f200d444e4df869335f01a6ee
DavidMartinez1x <dvidmartinezg@gmail.com>
2025-06-19 21:47:48 -0500

    prueba prueba.rb

---
+++ prueba.rb
@@ -0,0 +1,15 @@
+class Libro
+    attr_accessor :nombre,:fecha_pub, :autor
+        
+    def initialize (nombre,fecha_pub, autor)
+        @nombre = nombre
+        @fecha_pub = fecha_pub
+        @autor = autor
+    end
+
+    def descripcion
+     "El libro #{nombre} fue publicado en #{fecha_pub} por el autor '#{autor}'"
+end
+
+libro1 = Libro.new('Benito Camelo','1 de mato 2001','Juan Diaz')
+puts libro1.descripcion
